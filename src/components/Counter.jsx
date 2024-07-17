0693c3f94d6db4be12034d2e71d1ec9fd5b04403
ShakilOfficials <mrshakilhossainofficial@gmail.com>
2024-07-17 12:29:04 +0600

    created counter app with redux

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,36 @@
-import { useSelector } from "react-redux";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  increment,
+  decrement,
+  incrementByValue,
+} from "../redux/features/Counter/CounterSlice";
 const Counter = () => {
   const count = useSelector((state) => state.counter.count);
-  console.log(count);
+  const dispatch = useDispatch();
+
   return (
-    <div>
-      <button>Increment</button>
+    <div className="w-2/3 flex gap-5 mx-auto py-28 space-y-6">
+      <button
+        onClick={() => dispatch(incrementByValue(5))}
+        className="bg-sky-700 px-3 py-2 rounded-lg"
+      >
+        Increment By 5
+      </button>
+      <button
+        onClick={() => dispatch(increment())}
+        className="bg-sky-400 px-3 py-2 rounded-lg"
+      >
+        Increment
+      </button>
       <div>
-        <h1>0</h1>
+        <h1>{count}</h1>
       </div>
-      <button>Decrement</button>
+      <button
+        onClick={() => dispatch(decrement())}
+        className="bg-rose-400 px-3 py-2 rounded-lg"
+      >
+        Decrement
+      </button>
     </div>
   );
 };
